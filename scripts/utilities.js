d2385806d1a56fbe1d04d2d6627175b817f29178
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2022-06-17 15:11:39 -0300

    :sparkles: added adicional files web code

---
+++ scripts/utilities.js
@@ -0,0 +1,24 @@
+
+var dropdown = document.getElementsByClassName("dropdown");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+function copyCode(str) {
+  function listener(e) {
+    e.clipboardData.setData("text/plain", str);
+    e.preventDefault();
+  }
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+};
