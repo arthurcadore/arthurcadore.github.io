4586205545afcabe2ab12d187c04a05866937c77
arthurcadore <arthurbarcella.ifsc@gmail.com>
2026-04-15 11:11:22 -0300

    Updates

---
+++ assets/my-page-D7-qJ-_W.js
@@ -0,0 +1,1 @@
+import{I as o}from"./index-BOxu5sPj.js";const t=o;export{t as component};
--- assets/software-zAkqaLxP.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-nZLcIn-z.js";const n=()=>t.jsx(o,{});export{n as component};
