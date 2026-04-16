bbddba077ac34b36c2a38e66b0af351c62ca29f4
arthurcadore <arthurbarcella.ifsc@gmail.com>
2026-04-16 00:21:36 -0300

    Updates

--- assets/my-page-5cRZv-Yt.js
+++
@@ -1,1 +0,0 @@
-import{I as o}from"./index-Bm15m-ss.js";const t=o;export{t as component};
--- assets/software-DO_5amB8.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-Bm15m-ss.js";const n=()=>t.jsx(o,{});export{n as component};
